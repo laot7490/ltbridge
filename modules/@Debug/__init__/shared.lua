30ba08b651152ced0a9d001804808d5a07a4d179
laot7490 <fadeemir@gmail.com>
2026-05-02 23:39:22 +0300

    feat: Added type checks and safety formatting to Debug module.

--- modules/@Debug/__init__/shared.lua
+++ modules/@Debug/__init__/shared.lua
@@ -1,21 +1,55 @@
 local debugMode = 1
 local print = print
-local format = string.format
+local string_format = string.format
 
-local function canSend(_type)
-    if debugMode == 0 then return false
-    elseif debugMode == 1 and (_type ~= 'error') then return false
-    elseif debugMode == 2 and (_type ~= 'error' and _type ~= 'warning') then return false
-    elseif debugMode == 3 and (_type == 'verbose') then return false end
-    return true
+local function handleException(result, value)
+    if type(value) == 'function' then
+        return tostring(value)
+    end
+    return result
+end
+
+--- Format the data to a string.
+--- @param ... any Data to format.
+--- @return string Formatted data.
+local function format(message, ...)
+    local data = { ... }
+
+    for i = 1, #data do
+        local v = data[i]
+
+        if v == nil then
+            data[i] = 'nil'
+
+        elseif type(v) == "table" then
+            data[i] = json.encode(v, {
+                sort_keys = true,
+                indent = true,
+                exception = handleException
+            })
+
+        elseif type(v) == "boolean" then
+            data[i] = v and "true" or "false"
+
+        elseif type(v) ~= "string" then
+            data[i] = tostring(v)
+        end
+    end
+
+    return string_format(message, table.unpack(data))
 end
 
 --- Set debug mode.
 --- **0**: `Disabled` (Not Recommended)
+--- 
 --- **1**: `Errors` (Default)
+--- 
 --- **2**: `Errors + Warnings`
+--- 
 --- **3**: `Except Verbose`
+--- 
 --- **4**: `All Messages`
+--- 
 --- @param mode? number
 --- @ltbridge export: SetMode
 function SetDebugMode(mode)
@@ -23,11 +57,11 @@ function SetDebugMode(mode)
 end
 
 local debugTypes <const> = {
-   ['success'] = '^2[SUCCESS]^7',
-   ['error'] = '^1[ERROR]^7',
-   ['warning'] = '^3[WARNING]^7',
-   ['info'] = '^4[INFO]^7',
-   ['verbose'] = '^5[VERBOSE]^7',
+    ['success'] = { mode = 3, color = 2 },
+    ['error'] = { mode = 1, color = 1 },
+    ['warning'] = { mode = 2, color = 3 },
+    ['info'] = { mode = 3, color = 4 },
+    ['verbose'] = { mode = 4, color = 5 },
 }
 
 --- Prints a formatted and detailed (with file and line info) debug message.
@@ -37,17 +71,18 @@ local debugTypes <const> = {
 --- @ltbridge export: Detailed
 function DebugDetailed(message, type, ...)
     if not type then type = 'verbose' end
-    if not canSend(type) then return end
-    local prefix = debugTypes[type]
+    local data = debugTypes[type]
+    if not data then return end
+    if tonumber(debugMode) < tonumber(data.mode) then return end
     
     local info = debug.getinfo(2, 'Sl')
     local source = info and info.short_src or 'unknown'
     local line = info and info.currentline or -1
-
+    
     source = source:gsub("^@", "")
     local short = source:match("([^/]+/[^/]+%.lua)$") or source:match("([^/]+%.lua)$") or source
-
-    print(format('^3[%s:%d]^7 %s %s^7', short, line, prefix, format(message, ...)))
+    
+    print(string_format('^3[%s:%d]^7 ^%d[%s]^7 %s^7', short, line, data.color, type:upper(), format(message, ...)))
 end
 
 --- Prints a formatted debug message.
@@ -57,10 +92,11 @@ end
 --- @ltbridge export: Print
 function DebugPrint(message, type, ...)
     if not type then type = 'verbose' end
-    if not canSend(type) then return end
-    local prefix = debugTypes[type]
+    local data = debugTypes[type]
+    if not data then return end
+    if tonumber(debugMode) < tonumber(data.mode) then return end
     
-    print(format('%s %s', prefix, format(message, ...)))
+    print(string_format('^%d[%s]^7 %s^7', data.color, type:upper(), format(message, ...)))
 end
 
 --- Send a info debug mesage.
